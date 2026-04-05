4c2533d3673352de61e3416b0bd738572ba4799e
Ritesh Ojha <153787230+ojharitesh@users.noreply.github.com>
2026-04-05 17:58:25 -0400

    Change base path in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import react from '@astrojs/react';
 
 export default defineConfig({
     site: 'https://ojharitesh.github.io',
-    base: '/PersonalPortfolio-',
+    base: '/',
     integrations: [react()],
     vite: {
         assetsInclude: ['**/*.glb']
