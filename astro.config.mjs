0600a637c8e7895edc9e4a542f334f7b2b6fc6b9
Ritesh Ojha <153787230+ojharitesh@users.noreply.github.com>
2026-04-05 17:54:00 -0400

    Add site and base configuration to astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,8 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 
 export default defineConfig({
+    site: 'https://ojharitesh.github.io',
+    base: '/PersonalPortfolio-',
     integrations: [react()],
     vite: {
         assetsInclude: ['**/*.glb']
